75182cc8ed8107eac1aeafab6a17d46289ba8b58
enzobanin <e.banin@aluno.ifsp.edu.br>
2026-06-24 22:37:48 -0300

    hospedando banco de dados com as variaveis de ambiente

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -1,11 +1,17 @@
 import mysql, {Connection, QueryError} from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const dbConfig = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'ifsp',
-    database: 'bancaCoxinha'
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 
 const mysqlConnection: Connection = mysql.createConnection(dbConfig);
