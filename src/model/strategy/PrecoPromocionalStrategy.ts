845ccb76ec2385a1ce06ceb94be9214d9d4229e6
enzobanin <e.banin@aluno.ifsp.edu.br>
2026-06-26 17:25:42 -0300

    retirando comentarios

--- src/model/strategy/PrecoPromocionalStrategy.ts
+++ src/model/strategy/PrecoPromocionalStrategy.ts
@@ -2,6 +2,6 @@ import { CalculoPrecoStrategy } from "./CalculoPrecoStrategy";
 
 export class PrecoPromocionalStrategy implements CalculoPrecoStrategy{
     public calcular(precoBase: number): number {
-        return precoBase - 2; //10% de desconto
+        return precoBase - 2; 
     }
 }
